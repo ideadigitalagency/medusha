ac263b24264d548989bd623d52a0bc56fbabece3
Eugene Bevziuk <bevziukeuge@icloud.com>
2018-09-19 14:52:01 +0300

    Adaptive basket page

--- wp-content/themes/medusha/page-backet.php
+++ wp-content/themes/medusha/page-backet.php
@@ -18,19 +18,19 @@ Template Name: Корзина
 
             <div class="basket-table-head basket-table-head__padd">
                 <div class="row">
-                    <div class="col-lg-2">
+                    <div class="col-lg-2 col-md-2">
                         <div class="basket-table-head__item">Товары</div>
                     </div>
-                    <div class="col-lg-2">
+                    <div class="col-lg-2 col-md-2">
                         <div class="basket-table-head__item basket-table-head__item_padd-l">Кол-во</div>
                     </div>
-                    <div class="col-lg-3 align-center">
+                    <div class="col-lg-3 col-md-3 align-center">
                         <div class="basket-table-head__item align-center">Выбранный объем</div>
                     </div>
-                    <div class="col-lg-2 align-center">
+                    <div class="col-lg-2 col-md-2 align-center">
                         <div class="basket-table-head__item align-center">Вкус</div>
                     </div>
-                    <div class="col-lg-3 align-right">
+                    <div class="col-lg-3 col-md-3 align-right">
                         <div class="basket-table-head__item">Стоимость заказа</div>
                     </div>
                 </div>
@@ -40,12 +40,12 @@ Template Name: Корзина
 
                 <div class="basket-table-body__item basket-table-body__item_padd">
 					<div class="basket-table-body__close">
-						<img src="build/images/close.svg" alt="">
+						<img src="<?php echo get_template_directory_uri(); ?>/build/images/close.svg" alt="">
 					</div>
                     <div class="row no-gutters align-items-center">
-                        <div class="col-lg-2 align-center">
+                        <div class="col-lg-2 col-md-2 col-sm-2 col-6 align-center ">
                             <div class="basket-table__img">
-                                <img src="build/images/category-page/prod-1.png" alt="" class="img-fluid">
+                                <img src="<?php echo get_template_directory_uri(); ?>/build/images/category-page/prod-1.png" alt="" class="img-fluid">
                             </div>
                             <div class="basket-table__name basket-table__name_padd">
                                 Шестигранник
@@ -58,20 +58,20 @@ Template Name: Корзина
 								<span class="basket-table__price-text">шт</span>
                             </div>
 						</div>
-						<div class="col-lg-2 align-center">
+						<div class="col-lg-2 col-md-2 col-sm-2 table-body__item table-body__item_one  align-center">
 							<div class="basket-table__number-item quantity-block">
-								<button class="quantity-arrow-minus"> <img src="build/images/minus.svg" alt=""> </button>
+								<button class="quantity-arrow-minus"> <img src="<?php echo get_template_directory_uri(); ?>/build/images/minus.svg" alt=""> </button>
 								<input id="quantity-num1" class="quantity-num" type="number" value="1" />
-								<button class="quantity-arrow-plus"> <img src="build/images/plus.svg" alt=""> </button>
+								<button class="quantity-arrow-plus"> <img src="<?php echo get_template_directory_uri(); ?>/build/images/plus.svg" alt=""> </button>
 							</div>
 						</div>
-						<div class="col-lg-3 align-center">
+						<div class="col-lg-3 col-md-3 col-sm-3 table-body__item table-body__item_two  align-center">
 							<div class="basket-table__amound">1 л</div>
 						</div>
-						<div class="col-lg-2 align-center">
+						<div class="col-lg-2 col-md-2 col-sm-2 table-body__item table-body__item_three  align-center">
 							<div class="basket-table__taste basket-table-head__item_padd-l">Липа</div>
 						</div>
-						<div class="col-lg-3 align-right">
+						<div class="col-lg-3 col-md-3 col-sm-3 table-body__item table-body__item_four align-right">
 							<div class="basket-table__price-item">1097 <span class="basket-table__price-item_s">грн</span></div>
 						</div>
                     </div>
@@ -80,89 +80,89 @@ Template Name: Корзина
 
 				<div class="basket-table-body__item basket-table-body__item_padd">
 					<div class="basket-table-body__close">
-						<img src="build/images/close.svg" alt="">
+						<img src="<?php echo get_template_directory_uri(); ?>/build/images/close.svg" alt="">
 					</div>
-					<div class="row no-gutters align-items-center">
-						<div class="col-lg-2 align-center">
-							<div class="basket-table__img">
-								<img src="build/images/category-page/prod-2.png" alt="" class="img-fluid">
-							</div>
-							<div class="basket-table__name basket-table__name_padd">
-								Шестигранник
-							</div>
-							<div class="basket-table__price-one basket-table__price-one_padd">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col-lg-2 col-md-2 col-sm-2 col-6 align-center ">
+                            <div class="basket-table__img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/build/images/category-page/prod-2.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="basket-table__name basket-table__name_padd">
+                                Шестигранник
+                            </div>
+                            <div class="basket-table__price-one basket-table__price-one_padd">
 								<span class="basket-table__price-for-one">200 </span>
 								<span class="basket-table__price-for-one basket-table__price-for-one_small">грн</span>
 								<span class="basket-table__price-text">за </span>
 								<span class="basket-table__price-text_big">1 </span>
 								<span class="basket-table__price-text">шт</span>
-							</div>
+                            </div>
 						</div>
-						<div class="col-lg-2 align-center">
+						<div class="col-lg-2 col-md-2 col-sm-2 table-body__item table-body__item_one  align-center">
 							<div class="basket-table__number-item quantity-block">
-								<button class="quantity-arrow-minus"> <img src="build/images/minus.svg" alt=""> </button>
-								<input id="quantity-num2" class="quantity-num" type="number" value="1" />
-								<button class="quantity-arrow-plus"> <img src="build/images/plus.svg" alt=""> </button>
+								<button class="quantity-arrow-minus"> <img src="<?php echo get_template_directory_uri(); ?>/build/images/minus.svg" alt=""> </button>
+								<input id="quantity-num1" class="quantity-num" type="number" value="1" />
+								<button class="quantity-arrow-plus"> <img src="<?php echo get_template_directory_uri(); ?>/build/images/plus.svg" alt=""> </button>
 							</div>
 						</div>
-						<div class="col-lg-3 align-center">
+						<div class="col-lg-3 col-md-3 col-sm-3 table-body__item table-body__item_two  align-center">
 							<div class="basket-table__amound">1 л</div>
 						</div>
-						<div class="col-lg-2 align-center">
+						<div class="col-lg-2 col-md-2 col-sm-2 table-body__item table-body__item_three  align-center">
 							<div class="basket-table__taste basket-table-head__item_padd-l">Липа</div>
 						</div>
-						<div class="col-lg-3 align-right">
-							<div class="basket-table__price-item">908 <span class="basket-table__price-item_s">грн</span></div>
+						<div class="col-lg-3 col-md-3 col-sm-3 table-body__item table-body__item_four align-right">
+							<div class="basket-table__price-item">1097 <span class="basket-table__price-item_s">грн</span></div>
 						</div>
-					</div>
-					
+                    </div>
+                    
 				</div>
 
 				<div class="basket-table-body__item basket-table-body__item_padd">
 					<div class="basket-table-body__close">
-						<img src="build/images/close.svg" alt="">
+						<img src="<?php echo get_template_directory_uri(); ?>/build/images/close.svg" alt="">
 					</div>
-					<div class="row no-gutters align-items-center">
-						<div class="col-lg-2 align-center">
-							<div class="basket-table__img">
-								<img src="build/images/category-page/prod-3.png" alt="" class="img-fluid">
-							</div>
-							<div class="basket-table__name basket-table__name_padd">
-								Шестигранник
-							</div>
-							<div class="basket-table__price-one basket-table__price-one_padd">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col-lg-2 col-md-2 col-sm-2 col-6 align-center ">
+                            <div class="basket-table__img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/build/images/category-page/prod-1.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="basket-table__name basket-table__name_padd">
+                                Шестигранник
+                            </div>
+                            <div class="basket-table__price-one basket-table__price-one_padd">
 								<span class="basket-table__price-for-one">200 </span>
 								<span class="basket-table__price-for-one basket-table__price-for-one_small">грн</span>
 								<span class="basket-table__price-text">за </span>
 								<span class="basket-table__price-text_big">1 </span>
 								<span class="basket-table__price-text">шт</span>
-							</div>
+                            </div>
 						</div>
-						<div class="col-lg-2 align-center">
+						<div class="col-lg-2 col-md-2 col-sm-2 table-body__item table-body__item_one  align-center">
 							<div class="basket-table__number-item quantity-block">
-								<button class="quantity-arrow-minus"> <img src="build/images/minus.svg" alt=""> </button>
-								<input id="quantity-num2" class="quantity-num" type="number" value="1" />
-								<button class="quantity-arrow-plus"> <img src="build/images/plus.svg" alt=""> </button>
+								<button class="quantity-arrow-minus"> <img src="<?php echo get_template_directory_uri(); ?>/build/images/minus.svg" alt=""> </button>
+								<input id="quantity-num1" class="quantity-num" type="number" value="1" />
+								<button class="quantity-arrow-plus"> <img src="<?php echo get_template_directory_uri(); ?>/build/images/plus.svg" alt=""> </button>
 							</div>
 						</div>
-						<div class="col-lg-3 align-center">
+						<div class="col-lg-3 col-md-3 col-sm-3 table-body__item table-body__item_two  align-center">
 							<div class="basket-table__amound">1 л</div>
 						</div>
-						<div class="col-lg-2 align-center">
+						<div class="col-lg-2 col-md-2 col-sm-2 table-body__item table-body__item_three  align-center">
 							<div class="basket-table__taste basket-table-head__item_padd-l">Липа</div>
 						</div>
-						<div class="col-lg-3 align-right">
-							<div class="basket-table__price-item">19208 <span class="basket-table__price-item_s">грн</span></div>
+						<div class="col-lg-3 col-md-3 col-sm-3 table-body__item table-body__item_four align-right">
+							<div class="basket-table__price-item">1097 <span class="basket-table__price-item_s">грн</span></div>
 						</div>
-					</div>
-					
+                    </div>
+                    
 				</div>
 				
 			</div>
 			
 			<div class="basket-btn-wrp basket-btn-wrp_padd">
 				<div class="row no-gutters">
-					<div class="col-lg-6">
+					<div class="col-lg-6 col-12 mob-center">
 						<div class="basket-btn-wrp-item ">
 							<div class="basket-btn basket-btn_padd">
 								Продолжить покупку
@@ -170,7 +170,7 @@ Template Name: Корзина
 						</div>
 						
 					</div>
-					<div class="col-lg-6 align-right">
+					<div class="col-lg-6 col-12 mob-hidden align-right">
 						<div class="basket-btn-wrp-item">
 							<div class="basket-btn basket-btn_padd">
 								очистить корзину
